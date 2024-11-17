8ee23976c79dbcde2775510437c8ceebaebb2340
Isshiki-Iroha718 <2439165262@qq.com>
2024-11-17 15:14:41 -0500

    Modify the HTML and app.py files

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import os
-import openai
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
 
 app = Flask(__name__)
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
 def index():
